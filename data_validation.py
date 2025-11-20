96c7c909a238f6f5f5a2eb7dfe8d861594b258f2
Eugene <ymatviych@bnl.gov>
2025-11-20 16:45:12 -0500

    Update beamline acronym and change data stream access
    Template of validation script.

--- data_validation.py
+++ data_validation.py
@@ -5,7 +5,7 @@ from prefect import flow, task, get_run_logger
 from prefect.blocks.system import Secret
 from tiled.client import from_profile
 
-BEAMLINE_OR_ENDSTATION = "!!! Set the endstation or beamline_TLA here !!!"
+BEAMLINE_OR_ENDSTATION = "arpes"
 
 
 @task(retries=2, retry_delay_seconds=10)
@@ -13,13 +13,13 @@ def read_all_streams(uid, beamline_acronym=BEAMLINE_OR_ENDSTATION):
     logger = get_run_logger()
     api_key = Secret.load(f"tiled-{beamline_acronym}-api-key", _sync=True).get()
     tiled_client = from_profile("nsls2", api_key=api_key)
-    run = tiled_client[beamline_acronym]["raw"][uid]
+    run = tiled_client[beamline_acronym]["migration"][uid]
     logger.info(f"Validating uid {run.metadata['start']['uid']}")
     start_time = ttime.monotonic()
     for stream in run:
         logger.info(f"{stream}:")
         stream_start_time = ttime.monotonic()
-        stream_data = run[stream].read()
+        # stream_data = run[stream].read()
         stream_elapsed_time = ttime.monotonic() - stream_start_time
         logger.info(f"{stream} elapsed_time = {stream_elapsed_time}")
         logger.info(f"{stream} nbytes = {stream_data.nbytes:_}")
